d1913b87a38191e34053cded39a90f3aab81f0e0
CarlosLincon <carloslincon29@gmail.com>
2022-07-24 22:49:45 -0300

    Aprendendo State

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Formulario from './components/Formulario';
-import Botao from './components/Botao';
+import Lista from './components/Lista';
 
 function App() {
   return (
     <div className="App">
       <Formulario/>
+      <Lista/>
     </div>
   );
 }
